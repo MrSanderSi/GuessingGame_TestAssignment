63b31d80bebf451ccd485fab54092c5c8fe665da
MrSanderSi <sihversass@gmail.com>
2021-11-28 14:33:28 +0200

    implemented maven and moved files around to match the standard folder structure

--- Helmes_assignment/src/module-info.java
+++
@@ -1,8 +0,0 @@
-module Helmes_assignment {
-	requires javafx.controls;
-	requires javafx.graphics;
-	requires javafx.fxml;
-	requires javafx.base;
-	
-	opens application to javafx.graphics, javafx.fxml;
-}
